cdd66345cc851954a8f4f49953e1d28090cba8f9
Vojtech Hlavacka <hlavoj@gmail.com>
2023-03-29 21:48:09 +0200

    Original version from 2015

---
+++ FotoDateRanamer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+using log4net.Config;
+
+namespace FotoDateRanamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            var options = new Options();
+            if (Parser.Default.ParseArguments(args, options))
+            {
+                // Values are available here
+                Renamer renamer = new Renamer(Boolean.Parse(options.Debug), options.Lever, options.interval);
+                renamer.ReadDirectories(options.Folder);
+
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Options
+    {
+        [Option('f', "folder", Required = true, HelpText = "Folder for checking")]
+        public string Folder { get; set; }
+
+        [Option('l', "level", DefaultValue = 0, HelpText = "\n\t 0 - Rename only same avg and Directory date. \n\t 1 - Rename directory which hasn't name. \n\t 2 - avg and directory date is different, take avg. \n\t 3 - avg and directory date is different, take directory. \n ")]
+        public int Lever { get; set; }
+
+        [Option('i', "interval", DefaultValue = 10, HelpText = "Deferrence between avg and directory date (in days)")]
+        public int interval { get; set; }
+
+        [Option('d', "debug", DefaultValue = "true", HelpText = "Debuging only print renaming, but no change")]
+        public string Debug { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ FotoDateRanamer/Renamer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using log4net;
+
+namespace FotoDateRanamer
+{
+    public class Renamer
+    {
+        private readonly bool _debug;
+        private readonly int _level;
+        private readonly int _interval;
+        private const string DateFormat = "yyyy_MM_dd -";
+
+        ILog _logger = LogManager.GetLogger(typeof(Renamer));
+
+        public Renamer(bool debug, int level, int interval)
+        {
+            _debug = debug;
+            _level = level;
+            _interval = interval;
+        }
+
+        public List<string> ReadDirectories(string path)
+        {
+            //path = @"C:\School\fotos\test2\";
+            foreach (string s in Directory.GetDirectories(path))
+            {
+                string dirPath = s.Substring(0, s.LastIndexOf("\\")+1);
+                string dirName = s.Substring(s.LastIndexOf("\\")+1, s.Length - s.LastIndexOf("\\")-1);
+                try
+                {
+                    Tuple<DateTime, string> result;
+                    try
+                    {
+                        bool hasCorrectFormat = HasCorrectFormat(dirName);
+                        if (hasCorrectFormat)
+                        {
+                            _logger.Debug("Directory has correct format.");
+                            continue;
+                        }
+
+                        result = TryFindDate(dirName);
+                        DateTime fileDateTimeAvg = GetAvgFileTime(s);
+
+                        int tmpInterval = _interval;
+                        if (result.Item1.Day == 1 && result.Item1.Month == 1)
+                        {
+                            _logger.Debug("Directory date is only year.");
+                            tmpInterval = 365;
+                        }
+
+                        if (Math.Abs(fileDateTimeAvg.Date.Ticks - result.Item1.Ticks) > 864000000000 * tmpInterval)
+                        {
+                            
+                            _logger.ErrorFormat("Date dont match avrage date {0}", dirName);
+                            if (_level==2)
+                                RenameDir(s, dirPath + fileDateTimeAvg.ToString(DateFormat) + " " + dirName);
+                            if (_level==3)
+                                RenameDir(s, dirPath + result.Item1.ToString(DateFormat) + " " + result.Item2);
+
+                        }
+                        else
+                        {
+                            _logger.DebugFormat("Avrage datetime files is: {0} for folder: {1}", fileDateTimeAvg.ToString("dd.MM.yyyy"), dirName);
+                            RenameDir(s, dirPath + fileDateTimeAvg.ToString(DateFormat) + " " + result.Item2);
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        _logger.Warn("Folder has not date. folder: " + dirName);
+                        DateTime fileDateTimeAvg = GetAvgFileTime(s);
+                        _logger.DebugFormat("Avrage datetime files is: {0} for folder: {1}", fileDateTimeAvg.ToString("dd.MM.yyyy"), dirName);
+                        if(_level==1)
+                            RenameDir(s, dirPath + fileDateTimeAvg.ToString(DateFormat) + " " + dirName);
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+                    _logger.ErrorFormat("Dir {0} failed to rename.", s);
+
+                }
+
+            }
+            return null;
+        }
+
+        private bool HasCorrectFormat(string dirName)
+        {
+            if (dirName.Substring(10,1) == " " 
+                && dirName.Substring(11, 1) == "-"
+                && dirName.Substring(12, 1) == " "
+                )
+            {
+               string date = dirName.Substring(0, 12);
+                try
+                {
+                    DateTime.ParseExact(date, DateFormat, CultureInfo.InvariantCulture);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        private DateTime GetAvgFileTime(string s)
+        {
+            double sum = 0;
+            List<string> files = Directory.GetFiles(s,"*.jpg").ToList();
+            for (int i = 0; i < files.Count; i+=1)
+            {
+                sum += File.GetLastWriteTime(files[i]).Ticks / files.Count;
+            }
+
+            return new DateTime((long)sum);
+        }
+
+        private void RenameDir(string sourceDirName, string destDirName)
+        {
+            _logger.InfoFormat("destDirName: {0} -> sourceDirName: {1}", destDirName, sourceDirName);
+            if (!_debug) {
+                Directory.Move(sourceDirName, destDirName);
+            }
+        }
+
+        private Tuple<DateTime, string> TryFindDate(string s)
+        {
+            try
+            {
+                return TryFindDate1(s);
+            }
+            catch (Exception) { }
+
+            try
+            {
+                return TryFindDate2(s);
+            }
+            catch (Exception) { }
+            throw new Exception("Date not found");
+        }
+
+        private Tuple<DateTime, string> TryFindDate1(string s)
+        {
+
+            string oldDate = s.Substring(s.LastIndexOf(" "), s.Length - s.LastIndexOf(" "));
+            DateTime dt = TryGetDateTime(oldDate.Trim());
+
+            DateTime newDate = dt;
+            string nameWithoutDate = s.Substring(0, s.LastIndexOf(" "));
+            return new Tuple<DateTime, string>( newDate, nameWithoutDate);
+        }
+
+        private Tuple<DateTime, string> TryFindDate2(string s)
+        {
+
+            string oldDate = s.Substring(s.LastIndexOf(" "), s.Length - s.LastIndexOf(" "));
+            DateTime dt = new DateTime(Convert.ToInt32(oldDate.Trim()), 1, 1);
+
+            DateTime newDate = dt;
+            string nameWithoutDate = s.Substring(0, s.LastIndexOf(" "));
+            return new Tuple<DateTime, string>( newDate, nameWithoutDate);
+        }
+
+        private DateTime TryGetDateTime(string date)
+        {
+            try
+            {
+                return DateTime.Parse(date.Trim());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
